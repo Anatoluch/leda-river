92d1e74ea51f92b063d8c4ba673cdeaa5f3e50bb
Anatoluch <crimeagold@mail.ru>
2021-03-04 20:09:03 +0300

    19th commit

--- js/main.js
+++ js/main.js
@@ -26,4 +26,19 @@ overlay.addEventListener('click', function(){
    mobMenu.classList.remove('active');
    bodyEl.classList.remove('noscroll');
 });
+// Скрыть часть фото на мобильных устройствах \
+const galleryLink = document.querySelector('.gallery-link');//Ссылка показать/скрыть
+const photo = document.querySelectorAll('.hide-photo');//фотокарточка
+const link1 = document.querySelector('.link-1');// ссылка "показать все"
+const link2 = document.querySelector('.link-2');// ссылка "скрыть"
+
+if(galleryLink){
+   galleryLink.addEventListener('click', function(){
+      for(var i = 0; i < photo.length; i++) {
+         photo[i].classList.toggle('photo');
+      }
+      link1.classList.toggle('link-1-hide');
+      link2.classList.toggle('link-2-show');
+   })
+   }
    });
