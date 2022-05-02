f8a1648a355ca5b34ef18e658b0490c65c094120
Anatoluch <crimeagold@mail.ru>
2022-05-02 14:05:55 +0300

    02.05.22

--- js/owlCarusel-ctrl.js
+++ js/owlCarusel-ctrl.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
-  //Плагин owlCarusel (Отзывы)
-  $(".owl-carousel").owlCarousel({
-    items: 1,
-    nav: true,
-    navSpeed: 1000,
-    autoplaySpeed: 1200,
-    dots: false,
-    navText: ["", ""],
-    loop: true,
-    autoplay: true,
-    margin: 45,
-    autoplayTimeout: 8000,
-    autoplayHoverPause: true,
-  });
+$(document).ready(function () {
+	//Плагин owlCarusel (Отзывы)
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		nav: true,
+		navSpeed: 1000,
+		autoplaySpeed: 1200,
+		dots: false,
+		navText: ["", ""],
+		loop: true,
+		autoplay: true,
+		margin: 45,
+		autoplayTimeout: 8000,
+		autoplayHoverPause: true,
+	});
 });
